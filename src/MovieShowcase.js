45e2d2edbefd3012cd30b83988e038bbc95a4993
Joel Rivera <realjoelrivera@gmail.com>
2019-03-27 15:20:36 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, { Component } from "react";
+import MovieCard from "./card-components/MovieCard.js";
+import movieData from "./data.js";
 
 export default class MovieShowcase extends Component {
+  // MY METHOD
+  // generateMovieCards = () => {
+  //   return movieData.map((movie, idx) => {
+  //     return (
+  //       <MovieCard
+  //         key={idx}
+  //         movie={movie}
+  //         title={movie.title}
+  //         IMDBRating={movie.IMDBRating}
+  //         poster={movie.poster}
+  //         genres={movie.genres.forEach(genre => genre)}
+  //       />
+  //     );
+  //   });
+  // };
 
+  // FLATIRON'S METHOD
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
+    return movieData.map((data, idx) => <MovieCard key={idx} {...data} />);
+  };
 
   render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
+    return <div id="movie-showcase">{this.generateMovieCards()}</div>;
   }
 }
